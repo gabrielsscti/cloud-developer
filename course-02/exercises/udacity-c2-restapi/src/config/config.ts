6d05853d7736f72ea50a1561198c8ed0599c8388
Gabriel S. S. Costa <gabriel.costa@nuveo.ai>
2022-09-11 15:20:57 -0300

    fixes task

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   'dev': {
-    'username': '',
-    'password': '',
-    'database': '',
-    'host': '',
+    'username': 'udagram',
+    'password': '0g5RUxf1VY9FYfj5fPl6',
+    'database': 'udagram',
+    'host': 'udagram-dev.cr9ingzaggoo.us-east-1.rds.amazonaws.com',
     'dialect': 'postgres',
-    'aws_region': 'us-east-2',
-    'aws_profile': 'default',
-    'aws_media_bucket': 'udagram-ruttner-dev'
+    'aws_region': 'us-east-1',
+    'aws_profile': 'udagram-dev',
+    'aws_media_bucket': 'udagram-645854877916-dev'
   },
   'jwt': {
     'secret': ' '
